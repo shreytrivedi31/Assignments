24eb30e51fe98929749b0b60df30a1b9e16e6c6a
shreytrivedi31 <97449246+shreytrivedi31@users.noreply.github.com>
2022-01-12 16:40:30 +0530

    Add files via upload

---
+++ DBMS/Functions.sql
@@ -0,0 +1,15 @@
+select AVG(age) from Student
+
+select COUNT(name) from Student
+
+SELECT TOP 1 name FROM Student
+ORDER BY name ASC;
+
+SELECT TOP 1 name FROM Student
+ORDER BY name DESC;
+
+select sum(age) from Student
+
+select max(age) from Student
+
+select min(age) from Student
